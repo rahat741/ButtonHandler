4e9a64fec4874e327a6a6218a82c55f462c43555
rahat741 <rahatowezowprl@gmail.com>
2021-09-12 14:51:42 +0500

    button handler

--- ButtonHandler.cs
+++ ButtonHandler.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class ButtonHandler : MonoBehaviour
 {
-//Dunya
-    private DisplayImage currentDisplay;
     
+    private DisplayImage currentDisplay;
+    private float initialCameraSize;
+    private Vector3 initialCamaeraPosition;
     // Start is called before the first frame update
     void Start()
     {
         currentDisplay = GameObject.Find("DisplayerImage").GetComponent<DisplayImage>();
-        
+        initialCameraSize = Camera.main.orthographicSize;
+        initialCamaeraPosition = Camera.main.transform.position;
     }
 
     public void OnRinhtClikArrow()
@@ -22,6 +24,29 @@ public class ButtonHandler : MonoBehaviour
     {
         currentDisplay.CurrentWall = currentDisplay.CurrentWall - 1;
     }
+
+    public void OnClickZooomReturn()
+    {
+        if (currentDisplay.CurrentState == DisplayImage.State.zoom)
+        {
+            GameObject.Find("DisplayerImage").GetComponent<DisplayImage>().CurrentState = DisplayImage.State.normal;
+        var zoomInObjects = FindObjectsOfType<ZoomInOtject>();
+        foreach(var zoomInObject in zoomInObjects)
+        {
+            zoomInObject.gameObject.layer = 0;
+        }
+
+        Camera.main.orthographicSize = initialCameraSize;
+        Camera.main.transform.position = initialCamaeraPosition;
+        }
+        else
+        {
+            currentDisplay.GetComponent<SpriteRenderer>().sprite
+                = Resources.Load<Sprite>("Sprites/wall" + currentDisplay.CurrentWall);
+            currentDisplay.CurrentState = DisplayImage.State.normal;
+        }
+    }
+    
 
    
 }
